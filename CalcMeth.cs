55051e1e5b2aaf15f6b9ee2f398fd4bc78f6df22
Jesustpsk <dm.kuzmin11@mail.ru>
2023-10-15 01:35:29 +0300

    Initial Commit

---
+++ CalcMeth.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace CalculationMethods_L1_WPF;
+
+public static class CalcMeth
+{
+    public static double F(double x)
+    {
+        return 4 * x + Math.Pow(Math.E, x);
+    }
+
+    public static double FDerivative(double x)
+    {
+        return 4 + Math.Pow(Math.E, x);
+    }
+    public static double BisectionMethod(double a, double b, double epsilon)
+    {
+        if (F(a) * F(b) >= 0)
+        {
+            throw new ArgumentException("Функция должна иметь разные знаки на концах интервала");
+        }
+
+        var c = a;
+        while ((b - a) >= epsilon)
+        {
+            c = (a + b) / 2;
+        
+            if (Math.Abs(F(c)) < epsilon)
+            {
+                break;
+            }
+            else if (F(c) * F(a) < 0)
+            {
+                b = c;
+            }
+            else
+            {
+                a = c;
+            }
+        }
+
+        return c;
+    }
+    public static double NewtonMethod(double a, double b, double epsilon)
+    {
+        var x0 = (a + b) / 2;
+        var xN = F(x0);
+        var xN1 = xN - F(xN) / FDerivative(xN);
+        while (Math.Abs(xN1 - xN) > epsilon)
+        {
+            xN = xN1;
+            xN1 = xN - F(xN) / FDerivative(xN);
+        }
+        return xN1;
+    }
+
+    public static double CombinedMethod(double a, double b, double epsilon)
+    {
+        var x0 = a;
+        var x1 = b;
+        var fX0 = F(x0);
+        var fX1 = F(x1);
+        while (Math.Abs(fX1) > epsilon)
+        {
+            var x2 = x1 - fX1 * (x1 - x0) / (fX1 - fX0);
+            x0 = x1;
+            x1 = x2;
+            fX0 = fX1;
+            fX1 = F(x1);
+        }
+        return x1;
+    }
+
+    public static double ChordMethod(double a, double b, double epsilon)
+    {
+        if (F(a) * F(b) >= 0)
+        {
+            throw new ArgumentException("Функция должна иметь разные знаки на концах интервала");
+        }
+
+        var x = a;
+        double prevX;
+        do
+        {
+            prevX = x;
+            x -= (b - a) * F(x) / (F(b) - F(a));
+        
+            if (Math.Abs(F(x)) < epsilon)
+            {
+                break;
+            }
+            else if (F(x) * F(a) < 0)
+            {
+                b = x;
+            }
+            else
+            {
+                a = x;
+            }
+        }
+        while (Math.Abs(x - prevX) >= epsilon);
+
+        return x;
+    }
+    
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ScottPlot;
+using static CalculationMethods_L1_WPF.CalcMeth;
+using Color = System.Drawing.Color;
+
+namespace CalculationMethods_L1_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyPlot.Plot.AddVerticalLine(x: 0, color: Color.Black, width: 1);
+            MyPlot.Plot.AddHorizontalLine(y: 0, color: Color.Black, width: 1);
+        }
+
+        private void DrawFunction()
+        {
+            var x = new List<double>();
+            for (var i = -500.0; i < 500.0; i += 0.1)
+            {
+                x.Add(i / 100);
+            }
+            var y = x.Select(F).ToList();
+            for (var i = 1; i < x.Count; i++)
+            {
+                MyPlot.Plot.AddLine(x[i - 1], y[i - 1], x[i], y[i], Color.Lime);
+            }
+        }
+        private void ButtonStart_OnClick(object sender, RoutedEventArgs e)
+        {
+            MyPlot.Plot.Clear();
+            var a = Double.Parse(InputA.Text.Replace('.', ','));
+            var b = Double.Parse(InputB.Text.Replace('.', ','));
+            var tolerance = Double.Parse(InputT.Text.Replace('.', ','));
+            
+            DrawFunction();
+            MyPlot.Plot.AddVerticalLine(BisectionMethod(a, b, tolerance), Color.Red);
+            MyPlot.Plot.AddVerticalLine(NewtonMethod(a, b, tolerance), Color.Blue);
+            MyPlot.Plot.AddVerticalLine(CombinedMethod(a, b, tolerance), Color.Fuchsia);
+            MyPlot.Plot.AddVerticalLine(ChordMethod(a, b, tolerance), Color.Green);
+            
+            MyPlot.Plot.AddVerticalLine(x: 0, color: Color.Black, width: 1);
+            MyPlot.Plot.AddHorizontalLine(y: 0, color: Color.Black, width: 1);
+            
+            MyPlot.Refresh();
+        }
+    }
+}
